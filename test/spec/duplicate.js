4fb58601df0b91be0fa009d2c9158c55855b4718
Ain Tohvri <ain@flashbit.net>
2015-08-03 00:29:10 +0200

    Add duplicate method

--- test/spec/duplicate.js
+++ test/spec/duplicate.js
@@ -1,4 +1,4 @@
-/* global expect, it, beforeEach, afterEach, describe, xdescribe, xit */
+/* global expect, it, beforeEach, afterEach, describe, context */
 
 'use strict';
 
@@ -14,7 +14,7 @@
       nameGenerator: true
     };
 
-    xdescribe('duplicate', function() {
+    describe('duplicate', function() {
       beforeEach(function() {
         duplikaator = element.duplikaator(config).data(pluginName);
       });
@@ -23,20 +23,26 @@
         duplikaator.destroy();
       });
 
-      it('expected to duplicate source element into target', function() {
-        var source = $(element.attr('data-duplikaator-source')).html();
-        var target = $(element.attr('data-duplikaator-target')).html();
-        var expectation = source + target;
-        //console.log('trigger');
-        element.trigger('click.duplikaator');
-        //duplikaator.duplicate();
-        var newTarget = $(element.attr('data-duplikaator-target')).html();
-        return expect(newTarget).to.eql(expectation);
+      context('when called directly', function() {
+        it('expected to duplicate source element into target', function() {
+          var source = $(element.attr('data-duplikaator-source'))[0].outerHTML;
+          var target = $(element.attr('data-duplikaator-target')).html();
+          var expectation = target + source;
+          duplikaator.duplicate();
+          var newTarget = $(element.attr('data-duplikaator-target')).html();
+          return expect(newTarget).to.eql(expectation);
+        });
       });
 
-      xit('expected to duplicate source element', function() {
-        var fieldsets = $(element.attr('data-duplikaator-source'));
-        return expect(fieldsets.length).to.eql(2);
+      context('when triggered over handler', function() {
+        it('expected to duplicate source element into target', function() {
+          var source = $(element.attr('data-duplikaator-source'))[0].outerHTML;
+          var target = $(element.attr('data-duplikaator-target')).html();
+          var expectation = target + source;
+          element.trigger('click.duplikaator');
+          var newTarget = $(element.attr('data-duplikaator-target')).html();
+          return expect(newTarget).to.eql(expectation);
+        });
       });
 
     });
